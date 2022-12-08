9ece3f59b0d11065968b45fc0479af4f7b76fa21
Patrick Cohenn <patrickcohenn@gmail.com>
2022-12-08 07:26:56 -0300

    Iniciando um Calendário.

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,10 +1,20 @@
-var timeDisplay = document.getElementById("time");
-
-
-function refreshTime() {
-    var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
-    var formattedString = dateString.replace(", ", " - ");
-    timeDisplay.innerHTML = formattedString;
+function time()
+{
+today=new Date();
+h=today.getHours();
+h=("0" + h).slice(-2);
+m=today.getMinutes();
+m=("0" + m).slice(-2);
+document.getElementById('timeH').innerHTML=h+":"+m;
+setTimeout('time()',500);
 }
 
-setInterval(refreshTime, 1000);
+
+function abrir(URL) {
+    window.open(URL, 'janela', 'width=auto, height=auto, top=100, left=699, scrollbars=no, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')
+}
+
+function abrir2() {
+    URL="https://uol.com.br";
+    window.open(URL, 'janela', 'width=660, height=510, top=100, left=699, scrollbars=yes, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')
+}
