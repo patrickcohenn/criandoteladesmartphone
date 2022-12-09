ba54f2ebd84fe28358e2563eceaecbbf2d0e4ba7
Patrick Cohenn <patrickcohenn@gmail.com>
2022-12-09 09:36:47 -0300

    Form. para email

--- static/JS/script.js
+++ static/JS/script.js
@@ -18,3 +18,7 @@ function abrir2() {
     URL="https://uol.com.br";
     window.open(URL, 'janela', 'width=660, height=510, top=100, left=699, scrollbars=yes, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')
 }
+
+function msg() {
+    alert("E-mail encaminhado..");
+}
